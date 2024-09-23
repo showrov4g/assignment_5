b056ce011da8ddf4dac5d67da9390db7612ffd15
showrov4g <showrovg00@gmail.com>
2024-09-23 23:29:36 +0600

    Second section complete

--- js/index.js
+++ js/index.js
@@ -1,28 +1,94 @@
-document
-  .getElementById("flood-donate-button")
+// document
+//   .getElementById("flood-donate-button")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     let floodDonateInput = selectWithId("donate-input");
+//     let floodDonate = totalDonation("flood-donate");
+//     let myBalance = totalDonation("my-balance");
+//     let floodDonateBalance = floodDonateInput + floodDonate;
+//     let myCurrentBalance = myBalance - floodDonateInput;
+//     let donateBalance = (document.getElementById("flood-donate").innerText =
+//       floodDonateBalance);
+      
+//     let myBalanceAfterDonate = (document.getElementById("my-balance").innerText = myCurrentBalance);
+//     let popupBalance = document.getElementById('popup-balance').innerText = floodDonateInput;
+
+
+//     if (floodDonateInput <= 0 || isNaN(floodDonateInput)) {
+//       alert("Please provide correct information");
+//       let myBalanceAfterDonate = document.getElementById("my-balance").innerText = myBalance;
+//       let donateBalance = document.getElementById("flood-donate").innerText =
+//         floodDonate;
+//     }
+
+//     if (myBalance < floodDonateInput || myBalance <= 0) {
+//       alert("Your Balance is low");
+      
+//       let myBalanceAfterDonate = document.getElementById("my-balance").innerText = myBalance;
+//       let donateBalance = document.getElementById("flood-donate").innerText =
+//         floodDonate;
+//         // popup 
+//         let popup = document.getElementById('popup').classList.add('hidden');
+//         return;
+//     }
+//     // popup item 
+
+
+//     if (floodDonateInput > 0){
+//       let popup = document.getElementById('popup').classList.remove('hidden');
+//     }
+//   });
+
+
+  // fani flood dontaion section 
+
+  document
+  .getElementById("feni-flood-donate-button")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    let floodDonateInput = selectWithId("donate-input");
-    let floodDonate = totalDonation("flood-donate");
+    let floodDonateInput = selectWithId("feni-flood-donate-input");
+    let floodDonate = totalDonation("feni-flood-donate");
     let myBalance = totalDonation("my-balance");
     let floodDonateBalance = floodDonateInput + floodDonate;
     let myCurrentBalance = myBalance - floodDonateInput;
-    let donateBalance = (document.getElementById("flood-donate").innerText = floodDonateBalance);
-    let myBalanceAfterDonate = (document.getElementById('my-balance').innerText = myCurrentBalance);
+    let donateBalance = (document.getElementById("feni-flood-donate").innerText =
+      floodDonateBalance);
+      
+    let myBalanceAfterDonate = (document.getElementById("my-balance").innerText = myCurrentBalance);
+    let popupBalance = document.getElementById('popup-balance').innerText = floodDonateInput;
+
+
     if (floodDonateInput <= 0 || isNaN(floodDonateInput)) {
       alert("Please provide correct information");
+      let myBalanceAfterDonate = document.getElementById("my-balance").innerText = myBalance;
+      let donateBalance = document.getElementById("feni-flood-donate").innerText =
+        floodDonate;
     }
-    if(myBalance < floodDonateInput || myBalance <= 0){
-        alert("Your Balance is low");
-        let myBalanceAfterDonate = (document.getElementById('my-balance').innerText = myBalance);
-        let donateBalance = (document.getElementById("flood-donate").innerText = floodDonate);
+
+    if (myBalance < floodDonateInput || myBalance <= 0) {
+      alert("Your Balance is low");
+      
+      let myBalanceAfterDonate = document.getElementById("my-balance").innerText = myBalance;
+      let donateBalance = document.getElementById("feni-flood-donate").innerText =
+        floodDonate;
+        // popup 
+        let popup = document.getElementById('popup').classList.add('hidden');
+        return;
+    }
+    // popup item 
+
+
+    if (floodDonateInput > 0){
+      let popup = document.getElementById('popup').classList.remove('hidden');
     }
   });
 
-//   let x = 500;
 
-// let flood = document.getElementById('flood-donate');
-//  let fl= parseFloat(flood.innerText);
-//  let sum = fl+x;
-//  flood.innerText =sum;
-// console.log(sum);
+
+
+
+
+// popup close 
+document.getElementById('popup-close-button').addEventListener('click', function(){
+    let popup = document.getElementById('popup').classList.add('hidden');
+})
